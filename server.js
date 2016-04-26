d9b1a40b68db862bc99210b017e10d8169b32b91
supernoir <kawohl.bentodesign@gmail.com>
2016-04-26 23:45:05 +0200

    adds changes to server file

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // Prerequisites
 
 var Express = require('express');
-var server = Express();
+var http = require('http');
+var app = Express();
+
 
 
 
 var port = 7766;
-server.listen(port);
+
+app.listen(port);
+
+console.log('- - - -');
 console.log('Server running on port ' + port);
+console.log('- - - -');
 
 var startServer = function() {
     return port
