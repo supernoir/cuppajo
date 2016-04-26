057a2088fd2c92f0ea21aa08618c38e1f6b1bb59
supernoir <kawohl.bentodesign@gmail.com>
2016-04-26 19:47:30 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+// Prerequisites
+
+var Express = require('express');
+var server = Express();
+
+server.listen(8080);
---
+++ test/server_test.js
@@ -0,0 +1,7 @@
+// Unit testing for node server
+
+var Chai = require('chai').expect;
+
+var startServer = function() {
+   expect('server').to.exist;
+};
