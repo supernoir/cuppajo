6767a1836d830ceafafa3094a7d24285493cb0c9
supernoir <kawohl.bentodesign@gmail.com>
2016-05-10 20:25:22 +0200

    adds GET typesofcoffee, mongo dependencies, cors, body-parser

--- server.js
+++ server.js
@@ -4,24 +4,59 @@ var Express = require('express');
 var http = require('http');
 var app = Express();
 
-var PouchDB = require('pouchdb');
-var db = new PouchDB('cuppajo_db');
+var cors = require("cors");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+// --- CORS
+
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Methods", "POST, GET");  
+  next();     
+});
+
+
+// --- Nano
+
+var nano = require('nano')('http://localhost:5984')
+  , params   = {include_docs: true}
+  ;
+var cuppa = nano.db.use('cuppajo');
+
+
+// --- GET typesOfCoffee
+
+console.log('GET types of Coffee');
+app.get('/', function(request, response) {
+    
+        cuppa.list(params, function(err,body) {
+         var docs = [];
+           body.rows.forEach(function(doc) {
+               docs.push(doc);
+
+            });
+            response.json(docs);
+            console.error(err);
+            console.log(response);
+            console.log(body);
+
+        });  
+});
+
+
 
 
 // --- Start Server
 
 var port = 7766;
 app.listen(port);
-
-
-// --- Load JSON
-
-var typesOfCoffee = {
-    coffee_01: {
-        "name":"Espresso",
-        "milk":"none"
-    }
-}
 
 
 // --- Methods
@@ -31,6 +66,7 @@ var startServer = function() {
 };
 
 var getJSON = function() {
+    
     return typesOfCoffee
 };
 
